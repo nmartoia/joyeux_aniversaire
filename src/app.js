f0ecaf7100a1b2137e9641e871525128286e7742
nmartoia <nmartoia@edenschool.fr>
2022-03-22 10:51:59 +0100

    les 1000 id se creer

---
+++ src/app.js
@@ -0,0 +1,10 @@
+let hobby=['foot','jeux video','basket','code','echec','Jardinage','Randonnee','Jouer de la guitard','pêcher','tatouage','musculation','danser','astronomie','cuisine','camping','cyclisme','plombier','grillades','calligraphie','photographie']
+let a=[]
+for(let i=1;i<=1000;i++){
+    a.push({
+        id:i,
+        unsername:'username_'+i,
+        
+    })
+}
+console.log(a)
