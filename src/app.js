74ca3c4ba0a3337d25cd4579fa4b09812c489d1b
nmartoia <nmartoia@edenschool.fr>
2022-03-22 11:36:17 +0100

    les hobby sont deffinie au hasar mais peuve etre identique

--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
+import { v4 as uuidv4 } from 'uuid';
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
 let hobby=['foot','jeux video','basket','code','echec','Jardinage','Randonnee','Jouer de la guitard','pêcher','tatouage','musculation','danser','astronomie','cuisine','camping','cyclisme','plombier','grillades','calligraphie','photographie']
+function hobbyhasard(nrandom){
+    let t=[];
+    for(let i=0;i<nrandom;i++){
+        t.push(hobby[getRandomInt(19)])
+    }
+    return t;
+}
 let a=[]
 for(let i=1;i<=1000;i++){
+    
+    
     a.push({
-        id:i,
+        id:uuidv4(),
         unsername:'username_'+i,
-        
+        hobbys:hobbyhasard(getRandomInt(8)+3),
     })
 }
 console.log(a)
